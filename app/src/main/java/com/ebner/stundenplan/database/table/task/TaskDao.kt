4f7441e4fce9c673a9095d62c056acef3b1b7f65
Raphael Ebner <43086854+rafi0101@users.noreply.github.com>
2020-10-03 21:08:16 +0200

    Updated dependencies and fixed gradle warnings

--- app/src/main/java/com/ebner/stundenplan/database/table/task/TaskDao.kt
+++ app/src/main/java/com/ebner/stundenplan/database/table/task/TaskDao.kt
@@ -3,6 +3,7 @@ package com.ebner.stundenplan.database.table.task
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Query
+import androidx.room.RoomWarnings
 import androidx.room.Transaction
 import com.ebner.stundenplan.database.table.BaseDao
 import com.ebner.stundenplan.database.table.mergedEntities.TaskLesson
@@ -30,6 +31,7 @@ interface TaskDao : BaseDao<Task> {
     @Query("SELECT * FROM task INNER JOIN lesson ON task.tk_lid = lesson.lid INNER JOIN schoollesson ON lesson.l_slid = schoollesson.slid INNER JOIN subject ON lesson.l_sid = subject.sid INNER JOIN teacher ON subject.s_tid = teacher.tid INNER JOIN room ON subject.s_rid = room.rid INNER JOIN year ON task.tk_yid = year.yid WHERE task.tk_yid=:yid AND lesson.l_sid=:sid AND tkfinished=:finished ORDER BY tkdateyear ASC, tkdatemonth ASC, tkdateday ASC")
     fun getAllTaskBySubjectFinished(yid: Int, sid: Int, finished: Boolean): LiveData<List<TaskLesson>>
 
+    @SuppressWarnings(RoomWarnings.CURSOR_MISMATCH)
     @Transaction
     @Query("SELECT * FROM task INNER JOIN lesson ON lesson.lid = task.tk_lid INNER JOIN subject ON subject.sid = lesson.l_sid WHERE tk_yid=:yid AND subject.sid=:sid ORDER BY tkdateyear ASC, tkdatemonth ASC, tkdateday ASC")
     fun getSubjectTasks(yid: Int, sid: Int): LiveData<List<Task>>
